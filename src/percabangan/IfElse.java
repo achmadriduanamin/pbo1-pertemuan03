1b10a9a69a5c52f6d72e848af3de018aa8ac1e2b
achmadriduanamin <gokilrian12@gmail.com>
2021-04-25 19:39:02 +0800

    percabangan IfElse condition true sehingga statement2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,11 +2,14 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args){
-        double totalBelanja = 10000;
-        double uangDiDompet = 20000;
+        double totalBelanja = 20000;
+        double uangDiDompet = 10000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang Cukup, angsul : "+angsul);
         }else{
             System.out.println("Uang cukup... selamat menikmati..");
         }
