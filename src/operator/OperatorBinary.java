55459e96060bd494c5e79c91fb3309143ffacdc3
achmadriduanamin <gokilrian12@gmail.com>
2021-04-25 19:36:04 +0800

    operator binary aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Achmad Riduan Amin";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 4.00;
+
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
 }
