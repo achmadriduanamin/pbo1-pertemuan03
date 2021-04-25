c2ed2c159a73f6e7b1f1b5aa934dbc5a0dd3fd32
achmadriduanamin <gokilrian12@gmail.com>
2021-04-25 19:35:52 +0800

    operator binary assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Achmad Riduan Amin";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 4.00;
+}
